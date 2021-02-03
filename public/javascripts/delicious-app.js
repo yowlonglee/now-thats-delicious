46c04bd71b31819e5c4ca71ff0278a8e7b72e022
Yow-Long Lee <yowlonglee@frontendcafe.dev>
2021-02-03 15:05:37 +0800

    Add auto complete in address field

--- public/javascripts/delicious-app.js
+++ public/javascripts/delicious-app.js
@@ -1,3 +1,7 @@
 import '../sass/style.scss';
 
 import { $, $$ } from './modules/bling';
+import autocomplete from './modules/autocomplete';
+
+// Get the address, lat, lng fields from the page
+autocomplete( $('#address'), $('#lat'), $('#lng') );
