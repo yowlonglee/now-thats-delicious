62a4926001d10d0f870f60a2e0a497370778119f
Yow-Long Lee <yowlonglee@frontendcafe.dev>
2021-03-03 15:48:13 +0800

    Fix Google Map API method name

--- public/javascripts/modules/autocomplete.js
+++ public/javascripts/modules/autocomplete.js
@@ -1,8 +1,7 @@
 function autocomplete(input, latInput, lngInput) {
-  if (!input) return; // skip this fn from running if there is not input on the page
+  if (!input) return; // skip this fn from running if there is no input on the page
   const dropdown = new google.maps.places.Autocomplete(input);
-
-  dropdown.addEventListener('place_changed', () => {
+  dropdown.addListener('place_changed', function() {
     const place = dropdown.getPlace();
     console.log(place);
     latInput.value = place.geometry.location.lat();
