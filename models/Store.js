757077c95fef14d1250aaf83071e6dc3bbc76a31
Yow-Long Lee <yowlonglee@frontendcafe.dev>
2021-03-05 11:06:27 +0800

    Handle same store name with storeSchema pre hook

--- models/Store.js
+++ models/Store.js
@@ -35,12 +35,18 @@ const storeSchema = new mongoose.Schema({
   photo: String
 });
 
-storeSchema.pre('save', function(next) {
+storeSchema.pre('save', async function(next) {
   if (!this.isModified('name')) {
     next(); // skip it
     return; // stop this function from running
   }
   this.slug = slug(this.name);
+  // find other stores that have a slug of the same name, rename to slug-1, slug-2
+  const slugRegEx = new RegExp(`^(${this.slug})((-[0-9]*$)?)$`, 'i');
+  const storesWithSlug = await this.constructor.find({ slug: slugRegEx });
+  if (storesWithSlug.length) {
+    this.slug = `${this.slug}-${storesWithSlug.length + 1}`;
+  }
   next();
   // TODO make more resiliant so slugs are unique
 });
